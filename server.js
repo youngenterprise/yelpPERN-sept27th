fa0aa583b9d1e69908fba471d7937ca052d85f32
Nakaw <nakawyoung@gmail.com>
2021-09-28 10:10:05 -0400

    Added more to server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 import express from "express";
-const morgan=require("morgan")
-
+const morgan=require("morgan");
+const cors=require("cors");
+const db=require("./db/index");
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
