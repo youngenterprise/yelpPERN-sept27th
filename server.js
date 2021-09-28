fb3eb36162b064575f1260b2728c25916ea907bc
Nakaw <nakawyoung@gmail.com>
2021-09-28 10:58:35 -0400

    Added get all and get selected routes to express server.js

--- server.js
+++ server.js
@@ -10,3 +10,37 @@ app.use(cors());
 
 app.use(express.json());
 
+
+//get all
+app.get("/api/v1/restaurants",async (req,res)=>{
+    try {
+        const results=await db.query("SELECT * FROM restaurants");
+
+        res.status(200).json({
+            status:"Success",
+            results:results.rows.length,
+            data:{
+                restaurants:results.rows,
+            },
+        })
+
+    } catch (err) {
+        console.log(error);
+    }
+});
+
+//get selected restaurants
+app.get("/api/v1/restaurants/:id", async (req,res)=>{
+    console.log(req.params.id);
+    try {
+        const results=await db.query("select * from restaurants where id =$1",[req.params.id]);
+        res.status(200).json({
+            status:"success",
+            data:{
+                restaurant:results.rows[0],
+            }
+        });
+    } catch (err) {
+        console.log(err);        
+    }
+});
