422f872b21212388ffc91a2b0a1bf15753a2b2ec
Nakaw <nakawyoung@gmail.com>
2021-09-29 09:55:27 -0400

    Added create update delete routes to server.js

--- server.js
+++ server.js
@@ -46,7 +46,54 @@ app.get("/api/v1/restaurants/:id", async (req,res)=>{
 });
 
 //create a restaurant
+app.post("/api/v1/restaurants",async(req,res)=>{
+    console.log(req.body);
+    try {
+        const results=await db.query("INSERT INTO restaurants (name,location,price_range) VALUES ($1,$2,$3) returning *",
+            [req.body.name,req.body.location,req.body.price_range]);
+        console.log(results);
+        res.status(201).json({
+            status:"success",
+            data:{
+                restaurant:results.row[0],
+            }
+        });
 
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+//UPDATE
+app.put("/api/v1/restaurants/:id", async(req,res) => {
+    try {
+        const results = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+            [req.body.name, req.body.location, req.body.price_range, req.params.id]);
+        console.log(results);
+        res.status(200).json({
+            status:"Success",
+            data:{
+                restaurant:results.rows[0],
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+    console.log(req.params.id);
+    console.log(req.body);
+});
+
+//delete
+app.delete("/api/v1/restaurants/:id",async (req,res) =>{
+    try {
+        const result = await db.query("DELETE FROM restaurants where id = $1", [req.params.id]);
+        res.status(204).json({
+            status:"Success"
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 
 
